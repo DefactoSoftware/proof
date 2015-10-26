77744d9920f75d144e6172102f5857202416380b
GerwinV <g.veenstra@defacto.nl>
2015-10-26 16:22:22 +0100

    ADD requirementview and add mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#index"
   resources :users, only: [:index, :show, :edit, :update]
+  resources :requirements, only: [:index, :show, :create]
 end
---
+++ spec/controllers/requirement_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe RequirementController, type: :controller do
+
+end
