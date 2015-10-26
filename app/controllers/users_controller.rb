ba14583fefa05974d69ce2704cdf212c8cb9bf7e
Alexander Jeurissen <alexanderjeurissen@gmail.com>
2015-10-26 16:05:49 +0100

    Add new and create actions to User controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
+  skip_before_filter :authenticate!
+
   def index
     @users = User.all
   end
+
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = t("registrations.user.success")
+      redirect_to :root
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,39 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
   include Gravtastic
   gravtastic
+
   validates :email, presence: true
+
+  before_create :generate_confirmation_token
+
+  def password
+    @password ||= Password.new(self.encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
+  def confirm!
+    self.confirmation_token = nil
+    self.confirmed_at = Time.now.utc
+    self.save!
+  end
+
+  private
+
+  def generate_confirmation_token
+    loop do
+      token = SecureRandom.urlsafe_base64
+      unless User.where(:confirmation_token => token).any?
+        self.confirmation_token = token
+        self.confirmation_sent_at = Time.now.utc
+        break
+      end
+    end
+  end
 end
