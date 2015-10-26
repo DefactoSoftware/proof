661a9ee8fdd2e95301e37d5720c8782113afa1d4
GlennGeelen <g.geelen@defacto.nl>
2015-10-26 16:02:00 +0100

    add manager role to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :manager)
   end
 end
