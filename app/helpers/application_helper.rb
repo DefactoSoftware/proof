44caa53b1f9ef18295dcf2035cab508e6b54c6a1
Matthijs Kuiper <info@matthijskuiper.nl>
2015-10-30 15:54:55 +0100

    Evidence audit trail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     classes += " is-active" if current_page?(link_path)
     link_to link_text, link_path, class: classes
   end
+
+  def time_ago(time)
+    time_ago_in_words(time) + " ago"
+  end
 end
