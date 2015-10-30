afad42d04a622456e7fbae1375fbb11ad15c5573
Hugo Veeger <hugo@learningspaces.io>
2015-10-30 16:15:26 +0100

    add method to check if evidence is expired

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -18,4 +18,8 @@ class Evidence < ActiveRecord::Base
   belongs_to :requirement
   belongs_to :approver, class_name: "User", foreign_key: "approved_by_user_id"
   has_many :evidence_histories
+
+  def expired?
+    valid_until.past?
+  end
 end
